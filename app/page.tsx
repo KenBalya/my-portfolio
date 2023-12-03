877fd9593bd69456dae741471219fff79984b083
KenBalya <124903836+KenBalya@users.noreply.github.com>
2023-12-04 01:49:13 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="h-full w-full">
-      <div className="flex flex-col gap-20">
+      <div className="flex flex-col gap-20 space-y-6 md:space-y-10">
         <Hero />
         <Skills />
         <Awards/>
