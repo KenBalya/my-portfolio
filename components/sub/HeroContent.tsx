0c99b006669ac12bda8d44ea7a1a35c2f2e4624d
KenBalya <124903836+KenBalya@users.noreply.github.com>
2024-01-03 15:28:16 +0700

    Update HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -44,7 +44,7 @@ const HeroContent = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
-            Ken's Portfolio
+            Ken&apos;s Portfolio
           </h1>
         </motion.div>
 
